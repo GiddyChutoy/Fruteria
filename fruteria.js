b66109ec11ae424c33cbf44dcb11a3dc66b74403
adrian <giddychutoy@gmail.com>
2020-10-13 18:31:17 +0200

    [MOD] Rellenando codigo en las clases

---
+++ fruteria.js
@@ -0,0 +1,57 @@
+var arrayFrutas = [0,0,0,0,0,0,0,0,0,0];
+var dinero = 0;
+
+function sumarPlatanos(){
+    arrayFrutas[0] = (++arrayFrutas[0]);
+    console.log(arrayFrutas[0]);
+}
+
+function sumarCerezas(){
+    arrayFrutas[1] = (++arrayFrutas[1]);
+    console.log(arrayFrutas[1]);
+}
+
+function sumarUvas(){
+    arrayFrutas[2] = (++arrayFrutas[2]);
+    console.log(arrayFrutas[2]);
+}
+
+function sumarPiña(){
+    arrayFrutas[3] = (++arrayFrutas[3]);
+    console.log(arrayFrutas);
+}
+
+function sumarParaguaya(){
+    arrayFrutas[4] = (++arrayFrutas[4]);
+    console.log(arrayFrutas[4]);
+}
+
+function sumarManzana(){
+    arrayFrutas[5] = (++arrayFrutas[5]);
+    console.log(arrayFrutas[5]);
+}
+
+function sumarPera(){
+    arrayFrutas[6] = (++arrayFrutas[6]);
+    console.log(arrayFrutas[6]);
+}
+
+function sumarNaranja(){
+    arrayFrutas[7] = (++arrayFrutas[7]);
+    console.log(arrayFrutas[7]);
+}
+
+function sumarMelocoton(){
+    arrayFrutas[8] = (++arrayFrutas[8]);
+    console.log(arrayFrutas[8]);
+}
+
+function sumarKiwi(){
+    arrayFrutas[9] = (++arrayFrutas[9]);
+    console.log(arrayFrutas[9]);
+}
+
+function sumarHigos(){
+    arrayFrutas[10] = (++arrayFrutas[10]);
+    console.log(arrayFrutas[10]);
+}
